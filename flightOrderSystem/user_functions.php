c87a6a2a85c6ec9d92ab7d4fd58b348249500c59
Vinstarry <vinstarry@outlook.com>
2019-05-31 15:57:04 +0800

    add transcation on create_account

--- flightOrderSystem/user_functions.php
+++ flightOrderSystem/user_functions.php
@@ -43,6 +43,7 @@ final class User_functions {
      * @param string $tel:  user input telephone
      * @throws user_exception   some exception specified by Code
      */
+    const TRY_TIMES = 3;
     public static function create_account(mysqli &$link, string $name, string $psw, string $tel) {
         if (strlen($name) >= 30) {
             throw new user_exception(user_exception_codes::NameTooLong);
@@ -55,15 +56,32 @@ final class User_functions {
         }
         else {
             try {
-                $find_max_id_query = "select max(" . config\User_table::ID . ")" .
-                    " from " . config\User_table::NAME;
-                $result = $link->query($find_max_id_query);
-                list($maxid) = $result->fetch_row();
-                $id = ($maxid == null)? 60001 : $maxid + 1;
-                $insert_account = "insert into " . config\User_table::NAME .
-                    " values("."$id,'$name','$psw','$tel'". ");";
-                $link->query($insert_account, MYSQLI_STORE_RESULT);
-                if ($link->affected_rows == 0) {
+                $try_times = self::TRY_TIMES;
+                $succeeded = false;
+                do {
+                    $link->autocommit(false);
+                    $find_max_id_query = "select max(" . config\User_table::ID . ")" .
+                        " from " . config\User_table::NAME;
+                    $result = $link->query($find_max_id_query);
+                    list($maxid) = $result->fetch_row();
+                    $id = ($maxid == null) ? 60001 : $maxid + 1;
+                    $insert_account = "insert into " . config\User_table::NAME .
+                        " values(" . "$id,'$name','$psw','$tel'" . ");";
+                    $link->query($insert_account, MYSQLI_STORE_RESULT);
+                    if ($link->affected_rows != 0) {
+                        $link->commit();    // commit this transaction
+                        $succeeded = true;
+                        $link->autocommit(true);
+                        break;
+                    }
+                    else {
+                        $link->rollback();
+                        $link->autocommit(true);
+                    }
+                    $try_times--;
+                }while($try_times != 0);
+                if ($succeeded == false) {
+                    // Already tried 3 times but still failed
                     throw new user_exception(user_exception_codes::InsertAcconutFailed);
                 }
             }
