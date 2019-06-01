d43b67782f0305a85c6f8ed9db8a8706a8a9dfa0
Vinstarry <vinstarry@outlook.com>
2019-06-02 01:07:34 +0800

    change structure of Flight.php

--- flightOrderSystem/common/Flight.php
+++ flightOrderSystem/common/Flight.php
@@ -10,12 +10,14 @@ class Flight {
     private $arrive_place;
     private $begin_service_date;
     private $end_service_date;
-    private $seats_total;
-    private $seats_taken;
+    private $f_number;
+    private $e_number;
+    private $c_number;
 
     public function __construct($fid, $f_type, $depart_time,
                                 $duration, $depart_place, $arrive_place,
-                                $begin_service_date, $end_service_date, $seats_total) {
+                                $begin_service_date, $end_service_date,
+                                $f_number, $e_number, $c_number) {
         $this->fid = $fid;
         $this->f_type = $f_type;
         $this->depart_time = $depart_time;
@@ -24,14 +26,15 @@ class Flight {
         $this->arrive_place = $arrive_place;
         $this->begin_service_date = $begin_service_date;
         $this->end_service_date = $end_service_date;
-        $this->seats_total = $seats_total;
-        $this->seats_taken = 0;
+        $this->f_number = $f_number;
+        $this->e_number = $e_number;
+        $this->c_number = $c_number;
     }
 
     public function __toString() {
         return $this->fid . ",'" . $this->f_type . "','" .  $this->depart_time . "'," .  $this->duration . ",'" .
             $this->depart_place . "','" .  $this->arrive_place . "','" .  $this->begin_service_date . "','" .  $this->end_service_date . "'," .
-            $this->seats_total . "," . $this->seats_taken;
+            $this->f_number . "," . $this->e_number . "," . $this->c_number;
     }
 }
 
