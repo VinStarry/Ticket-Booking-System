6235b83355c473fba1e76c8c6e8ba54f7de9b1d0
Vinstarry <vinstarry@outlook.com>
2019-06-01 10:22:05 +0800

    change files' structure

--- flightOrderSystem/user/user_main.php
+++ flightOrderSystem/user/user_main.php
@@ -1,12 +1,12 @@
 <?php
-include_once 'config.php';
-include_once 'DBConnector.php';
+include_once '../common/config.php';
+include_once '../common/DBConnector.php';
 include_once 'user_functions.php';
 
 error_reporting(E_ALL & E_STRICT);
 
 $conn = new DBConnector();
 $user = User_functions::login_account($conn->link, 60001, 'tonybrown911');
-//echo "$user->UID : $user->UName : $user->UTelephone <br />"
+echo "$user->UID : $user->UName : $user->UTelephone : $". $user->getUBalance() . "<br />"
 
 ?>
